a115f721c8250a1fd607b6c6f1f7740c8996c98f
Galen Fletcher Evans <galevans@cisco.com>
2018-07-17 14:10:26 -0700

    add .random command

--- arstid.js
+++ arstid.js
@@ -31,16 +31,23 @@ bot.on('message', function (user, userID, channelID, message, evt) {
         var args = message.substring(7).split(' ');
         var cmd = args[0];
         args = args.splice(1);
+        var seasonCommand = ["summer", "fall", "winter", "spring"];
+
+        //random initalizer called via arstid.random chooses random season and sends season command
+        if (cmd == "random"){
+          var stuff = seasonCommand[choose()];
+          bot.sendMessage({to: channelID, message: "arstid." + stuff});
+        }
+
         //Commands for all seasons, if a season command comes in,
         //respond with randomized traits and flaws
-        var seasonCommand = ["summer", "fall", "winter", "spring"];
         for (var i=0, len = seasonCommand.length; i < len; i++) {
           if (cmd == seasonCommand[i]){
             var pt = choose();
             var flaw = choose();
             bot.sendMessage({ to: channelID, message: "I am "+ cmd +"! \nPersonality Trait: " +
-              eval("season."+cmd+".personalityTrait[pt]") + "\nFlaw: " +
-              eval("season."+cmd+".flaw[flaw]")});
+              eval("season."+seasonCommand[i]+".personalityTrait[pt]") + "\nFlaw: " +
+              eval("season."+seasonCommand[i]+".flaw[flaw]")});
           }
         };
     }
